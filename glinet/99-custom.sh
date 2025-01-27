0509a63568180b0090d6a9f64a8fd463b6884a6d
小乱 <121221628+wangjianyag@users.noreply.github.com>
2025-01-27 19:46:16 +0800

    Update 99-custom.sh

--- glinet/99-custom.sh
+++ glinet/99-custom.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # 该脚本为immortalwrt首次启动时 运行的脚本 即 /etc/uci-defaults/99-custom.sh
+
 # 设置默认防火墙规则，方便虚拟机首次访问 WebUI
 uci set firewall.@zone[1].input='ACCEPT'
 
@@ -10,12 +11,19 @@ uci set "dhcp.@domain[-1].ip=203.107.6.88"
 
 # 检查配置文件是否存在
 SETTINGS_FILE="/etc/config/pppoe-settings"
-if [ ! -f "$SETTINGS_FILE" ]; then
+if [! -f "$SETTINGS_FILE" ]; then
     echo "PPPoE settings file not found. Skipping." >> $LOGFILE
 else
-   # 读取pppoe信息(由build.sh写入)
-   . "$SETTINGS_FILE"
+  . "$SETTINGS_FILE"
 fi
+
+# 新增USB接口配置
+uci add network interface
+uci set network.@interface[-1].name='USB'
+uci set network.@interface[-1].proto='dhcp'
+uci set network.@interface[-1].device='ETH2'
+uci set network.@interface[-1].firewall_zone='wan wan6'
+
 # 无需判断网卡数量 因为glinet是多网口
 uci set network.lan.ipaddr='192.168.8.1'
 echo "set 192.168.8.1 at $(date)" >> $LOGFILE
@@ -24,11 +32,11 @@ echo "print enable_pppoe value=== $enable_pppoe" >> $LOGFILE
 if [ "$enable_pppoe" = "yes" ]; then
     echo "PPPoE is enabled at $(date)" >> $LOGFILE
     # 设置拨号信息
-    uci set network.wan.proto='pppoe'                
-    uci set network.wan.username=$pppoe_account     
-    uci set network.wan.password=$pppoe_password     
-    uci set network.wan.peerdns='1'                  
-    uci set network.wan.auto='1' 
+    uci set network.wan.proto='pppoe'
+    uci set network.wan.username=$pppoe_account
+    uci set network.wan.password=$pppoe_password
+    uci set network.wan.peerdns='1'
+    uci set network.wan.auto='1'
     echo "PPPoE configuration completed successfully." >> $LOGFILE
 else
     echo "PPPoE is not enabled. Skipping configuration." >> $LOGFILE
@@ -43,7 +51,7 @@ uci commit
 
 # 设置编译作者信息
 FILE_PATH="/etc/openwrt_release"
-NEW_DESCRIPTION="Compiled by wukongdaily"
+NEW_DESCRIPTION="Compiled by YuBai"
 sed -i "s/DISTRIB_DESCRIPTION='[^']*'/DISTRIB_DESCRIPTION='$NEW_DESCRIPTION'/" "$FILE_PATH"
 
 exit 0
